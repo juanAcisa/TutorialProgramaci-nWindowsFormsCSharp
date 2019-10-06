ef7fc5b4e596c97cd0c307815acfeea057a07bf7
juan sanchez <juan.acisa@gmail.com>
2019-10-06 20:21:23 +0200

    20191006D2021

--- Programa01_01/Form1.Designer.cs
+++ Programa01_01/Form1.Designer.cs
@@ -28,13 +28,37 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.btnSaludo = new System.Windows.Forms.Button();
+            this.lblSaludo = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // btnSaludo
+            // 
+            this.btnSaludo.Location = new System.Drawing.Point(188, 217);
+            this.btnSaludo.Name = "btnSaludo";
+            this.btnSaludo.Size = new System.Drawing.Size(247, 144);
+            this.btnSaludo.TabIndex = 0;
+            this.btnSaludo.Text = "Saludo";
+            this.btnSaludo.UseVisualStyleBackColor = true;
+            this.btnSaludo.Click += new System.EventHandler(this.BtnSaludo_Click);
+            this.btnSaludo.MouseLeave += new System.EventHandler(this.BtnSaludo_MouseLeave);
+            this.btnSaludo.MouseMove += new System.Windows.Forms.MouseEventHandler(this.BtnSaludo_MouseMove);
+            // 
+            // lblSaludo
+            // 
+            this.lblSaludo.Location = new System.Drawing.Point(183, 416);
+            this.lblSaludo.Name = "lblSaludo";
+            this.lblSaludo.Size = new System.Drawing.Size(264, 116);
+            this.lblSaludo.TabIndex = 1;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(634, 580);
+            this.Controls.Add(this.lblSaludo);
+            this.Controls.Add(this.btnSaludo);
+            this.Margin = new System.Windows.Forms.Padding(6);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -42,6 +66,9 @@
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnSaludo;
+        private System.Windows.Forms.Label lblSaludo;
     }
 }
 
--- Programa01_01/Form1.cs
+++ Programa01_01/Form1.cs
@@ -12,9 +12,35 @@ namespace Programa01_01
 {
     public partial class Form1 : Form
     {
+        static Point x;
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void BtnSaludo_Click(object sender, EventArgs e)
+        {
+            lblSaludo.Text = "     Hola mundo     ";
+        }
+
+        private void BtnSaludo_MouseLeave(object sender, EventArgs e)
+        {
+            lblSaludo.Text = "";
+        }
+
+        private void BtnSaludo_MouseMove(object sender, MouseEventArgs e)
+        {
+            try
+            {
+                if (btnSaludo.PointToScreen(Form1.x) > x)
+                    lblSaludo.Text = " " + lblSaludo.Text.Substring(0, lblSaludo.Text.Length - 1);
+                if (btnSaludo.PointToScreen < x)
+                    lblSaludo.Text = lblSaludo.Text.Substring(1, lblSaludo.Text.Length - 1) + " ";
+
+                x = btnSaludo.PointToScreen;
+            }
+            catch { }
         }
     }
 }
